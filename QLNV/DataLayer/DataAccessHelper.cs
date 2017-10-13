5a63b079d7077b87b1ad4f3ead5207af49c4816b
vyle3696 <vyle3696@gmail.com>
2017-10-13 19:51:37 +0700

    add dataaccesshelper

---
+++ QLNV/DataLayer/DataAccessHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace QLNV.DataLayer
+{
+    class DataAccessHelper
+    {
+        #region Data access properties
+        public SqlConnection con;
+        SqlCommand cmd;
+        public DataTable dt;
+
+        #endregion
+
+        #region Init properties
+        public DataAccessHelper()
+        {
+            String connectionString = @"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=QLNT;Data Source=VYLE";
+            con = new SqlConnection(connectionString);
+        }
+        #endregion
+
+        #region Procceed with database
+        public void Open()
+        {
+            if (con.State == System.Data.ConnectionState.Closed)
+                con.Open();
+        }
+
+        private void Close()
+        {
+            if (con.State == System.Data.ConnectionState.Open)
+                con.Close();
+        }
+
+        public DataTable GetDataTable(string select)
+        {
+            SqlDataAdapter da = new SqlDataAdapter(select, con);
+            dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+        #endregion
+
+        public SqlCommand Command(String commandString)
+        {
+            this.cmd = new SqlCommand(commandString, con);
+            return cmd;
+        }
+    }
+}
